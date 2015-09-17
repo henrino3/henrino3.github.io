39b99096a20380a09e9ad3124be74f7e42c6dfd6
Henry Mascot <henrino3@gmail.com>
2015-09-17 22:09:51 +0000

    Add new files

---
+++ examples/firstprogram.java
@@ -0,0 +1,30 @@
+
+
+	public class MyFirstJavaProgram {
+		
+		public static void main(String []args) {
+			System.out.println("Hello World");
+
+			
+			byte small1, small2, small3, small4, small5;
+
+			short bigger1, bigger2, bigger3, bigger4, bigger5;
+
+			int wholenumber1, wholenumber2, wholenumber3, wholenumber4, wholenumber5;
+
+			long biggernumber1, biggernumber2, biggernumber3, biggernumber4, biggernumber5;
+
+			float decimalnumber1, decimalnumber2, decimalnumber3, decimalnumber4, decimalnumber5;
+
+			double biggestnumber1, biggestnumber2, biggestnumber3, biggestnumber4, biggestnumber5;
+
+			char writtenword1, writtenword2, writtenword3, writtenword4, writtenword5;
+
+			boolean trueorfalse1, trueorfalse2,trueorfalse3,trueorfalse4,trueorfalse5;
+
+
+			int age= 100.3;
+
+		}
+	}
+
