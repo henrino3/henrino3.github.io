473fefab622bae053f82ff9fce176bacd73d228b
Henry Mascot <henrino3@gmail.com>
2016-01-22 05:58:00 +0000

    Finish Project

--- Site-builder101/js/app.js
+++ Site-builder101/js/app.js
@@ -1,67 +1,125 @@
 $( document ).ready(function() {
 
 
-    $('.side h1, .side img').draggable({
-      helper: 'clone'
-    });
+
+          $('.side h3, .side h5, .side img, .side p, .side table').draggable({
+            helper: 'clone'
+          });
 
 
-    $('.canvas h1').draggable({
-      helper: 'clone'
-    });
+          $('.canvas h1').draggable({
+            helper: 'clone'
+          });
 
 
+          $(document).dblclick(function(event) {
+              var elem = $(event.target);
+              elem.remove();
+          });
+
+          $('#download').on('click', function(){
+              saveToHtml();
+          })
 
 
+          createElement = function (id, classs){
+
+            if (id === 'tagline'){
+              data = "<h3 id='tagline'>Welcome to Henry's Site!</h3>";
+            };
+
+            if (id === 'title'){
+              data =  "<h5 id='title'> Personal Site</h5>";
+            };
+
+            if (id === 'logo'){
+              data =  "<img src='img/logo.png' alt='' id='logo'>";
+            };
+
+            if (id === 'descr'){
+              data =  "<p class='content' id='descr'>"+
+                   "Henry is a Frontend Engineer, Digital Marketer, Entrepreneur and "+
+                   "currently and EIT at MEST.Henry is a Co-founder of Hub10 and Velexo.</p>";
+            };
+
+            if (id === 'table'){
+              data =  "<table class='table table-bordered' id='table'>"+
+              "<thead><tr><th>First Name</th><th>Last Name</th>"+
+              "</tr></thead><tbody><tr><td>Henry</td>"+
+              "<td>Mascot</td></tr></tbody></table>";
+            };
+
+            $(classs).html(data);
+
+          };
+
+           function saveToHtml () {
+            var data = $('.canvas').html();
+               data = "<div class='row canvas' id='canvas'>"+data+"</div>";
+
+                console.log(data);
+
+                $("<a/>", {
+                    download: $.now() + ".html",
+                    href: URL.createObjectURL(
+                    new Blob([data], {
+                    type: "text/html"
+                    }))
+                })
+                .appendTo(".right")[0].click();
+
+                $(window).one("focus", function() {
+                    $("a").last().remove()
+                })
+
+          };
 
 
           $('.b-logo').sortable();
           $('.b-logo').droppable({
-          drop: function(ev, ui) {
-              console.log(ui);
-              console.log(ui.draggable.context);
-              console.log($(ui.draggable.context).data('pic'));
-              $('.b-logo').html(ui.draggable.context);
-            }});
+                    activeClass: "ui-state-default",
+                    hoverClass: "ui-state-hover",
+                    drop: function( event, ui ) {
+                        createElement(ui.draggable.attr("id"), '.b-logo')
+                      }
+          });
 
 
           $('.b-menu').sortable();
           $('.b-menu').droppable({
-          drop: function(ev, ui) {
-              $('.b-menu').html(ui.draggable.context);
-            }});
+            activeClass: "ui-state-default",
+            hoverClass: "ui-state-hover",
+            drop: function( event, ui ) {
+                createElement(ui.draggable.attr("id"), '.b-menu')
+              }
+            });
 
           $('.b-title').sortable();
           $('.b-title').droppable({
-          drop: function(ev, ui) {
-              $('.b-title').html(ui.draggable.context);
-            }});
+            activeClass: "ui-state-default",
+            hoverClass: "ui-state-hover",
+            drop: function( event, ui ) {
+                createElement(ui.draggable.attr("id"), '.b-title')
+              }
+            });
 
-          $('.body-main').sortable();
-          $('.body-main').droppable({
-          drop: function(ev, ui) {
-              $('.body-main').html(ui.draggable.context);
-            }});
+          $('.body-table').sortable();
+          $('.body-table').droppable({
+            activeClass: "ui-state-default",
+            hoverClass: "ui-state-hover",
+            drop: function( event, ui ) {
+                createElement(ui.draggable.attr("id"), '.body-table')
+              }
+            });
 
           $('.body-text').sortable();
           $('.body-text').droppable({
-          drop: function(ev, ui) {
-              $('.body-text').html(ui.draggable.context);
-            }});
-
-
-    // $('body').click(function(e) {
-    //       var target = $(e.target);
-    //       console.log(target);
-    // });​
-    //
-    // drag = function(e) {
-    //   e = "."+e;
-    //   console.log(e);
-    //
-    //     //put here
-    //     }
-
+            activeClass: "ui-state-default",
+            hoverClass: "ui-state-hover",
+            drop: function( event, ui ) {
+                createElement(ui.draggable.attr("id"), '.body-text')
+              }
+            });
 
 
 
